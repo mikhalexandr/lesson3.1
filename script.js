73512aec29fd278646e2a77b0669afd1311ef5ac
Alexandr <mikhalchenkov22@gmail.com>
2022-08-24 19:20:31 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let canvas = document.getElementById("sandbox"),
+    context = canvas.getContext("2d"),
+    line = new Path2D()
+
+line.moveTo(0, 0)
+line.lineTo(300, 300)
+
+context.lineWidth = 5
+context.stroke(line)
+
+context.fillStyle = "red"
+context.fillRect(0, 0, 200, 200)
+
+context.fillStyle = "rgba(0, 255, 0, 0.5)"
+context.fillRect(100, 100, 300, 300)
